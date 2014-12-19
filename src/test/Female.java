410a72b48efa838a7d3b02176f6d764e1dc08fe7
Nicholas Lindboe <NicholasLindboe@gmail.com>
2014-12-20 00:29:00 +0100

    added female

---
+++ src/test/Female.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Female extends Person {
+
+	public Female(String name, int age) {
+		super(name, age);
+	}
+
+	@Override
+	public void doStuff() {
+		System.out.println("Shops alot and spends alot of money");
+		
+	}
+
+}
